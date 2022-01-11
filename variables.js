bb23e9a82765504cfc498958ad73c05bc4c30ae4
Betul Baskan <btlbsk@hotmail.com>
2022-01-10 20:40:09 -0700

    variables

--- variables.js
+++ variables.js
@@ -1,6 +1,11 @@
-var petName = "Togo"
-var lastName = "Baskan"
-var petsAge = 1
+var petName = "Lily"
+var boyName = "Togo"
+var petsAge = "1"
+
 var numDogs = 2
+var dailySnackTimes = 3
+var wakingTime = 6
+
+var blackColor = false
 var isFunny = true
 var lovesPet = true
